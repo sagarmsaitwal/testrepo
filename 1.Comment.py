b4bad6861c7e5e20a5a280358069a39fa2c5b058
sagarmsaitwal <79134624+sagarmsaitwal@users.noreply.github.com>
2021-03-27 10:08:30 -0700

    Add files via upload

---
+++ 1.Comment.py
@@ -0,0 +1,14 @@
+""" Python Comments
+Comments can be used to explain Python code.
+
+Comments can be used to make the code more readable.
+
+Comments can be used to prevent execution when testing code."""
+#Example:
+"""Creating a Comment
+Comments starts with a #, and Python will ignore them:"""
+
+
+#this is comment
+x="sagar"
+print(x) #this is comment
